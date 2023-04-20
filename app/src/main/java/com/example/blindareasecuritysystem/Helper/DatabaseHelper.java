40c3414e57d0ee169419a0084c7beaa33ec328da
Blue <songofapple@outlook.com>
2023-04-20 17:29:10 +0800

    add .gitignore

---
+++ app/src/main/java/com/example/blindareasecuritysystem/Helper/DatabaseHelper.java
@@ -0,0 +1,28 @@
+package com.example.blindareasecuritysystem.Helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    // 建用户表
+    final String CREATE_TABLE = "CREATE TABLE user();";
+
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+}
